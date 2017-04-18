ce05f26b6c9f75d1bf698c3c62cfd891bfacf1d6
Balazs Nemeth <balazs.nemeth@tmit.bme.hu>
2017-04-18 13:28:30 +0200

    Add objective value plotting script

---
+++ simulation/create_obj_value_plots.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# usage: ./create_obj_value_plots.sh <<obj_values.csv>> <<simulation_log.err>> <<output file name>>
+
+cat $2 | grep -e "DEBUG: Hybrid Orchestrator:Setting online resource based on reoptimized resource for request" |
+cut -d ' ' -f12 | cut -d '-' -f3 | awk '{print $1,",",2}' > reopts.txt
+
+gnuplot <<- EOF
+    set xlabel "Number of simulation iterations"
+    set ylabel "Objective function value"
+    set term pdf
+    set datafile separator ","
+    set boxwidth 0.01
+    set output "$3.pdf"
+    plot "$1" using 1:4 with linespoint title "Total", "$1" using 1:2 with linespoint title "Link bandwidth component", \
+    "$1" using 1:3 with linespoint title "Load balancing component", "reopts.txt" using 1:2 with boxes title "Reoptimizations"
+EOF
+
+# rm reopts.txt
