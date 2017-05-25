c0dfcf2df0f95efea0a8eaae91457016ff0c0588
Balazs Nemeth <balazs.nemeth@tmit.bme.hu>
2017-05-25 16:42:35 +0200

    Add licence file headers

--- simulation/MappingSimulationFrameWork.py
+++ simulation/MappingSimulationFrameWork.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Balazs Nemeth
+# Copyright 2017 Balazs Nemeth, Mark Szalay, Janos Doka
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- simulation/ResourceGetter.py
+++ simulation/ResourceGetter.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Balazs Nemeth
+# Copyright 2017 Balazs Nemeth, Mark Szalay, Janos Doka
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Embedded file name: /home/dj/escape/mapping/simulation/ResourceGetter.py
 import copy
 import string
 from abc import ABCMeta, abstractmethod
